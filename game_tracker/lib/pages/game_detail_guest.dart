18f2a78df2df7e3f754f4a7c31b3523a654a6b4f
Rafael Tiribás <rafaeltiribas@outlook.com>
2024-07-10 17:24:12 -0300

    feat guest dashboard screen

---
+++ game_tracker/lib/pages/game_detail_guest.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:game_tracker/controllers/game_controller.dart';
+import 'package:game_tracker/models/game.dart';
+import 'package:game_tracker/pages/edit_game.dart';
+import 'package:game_tracker/pages/review_page.dart';
+
+class GameDetailGuest extends StatefulWidget {
+  final VoidCallback signOut;
+  final int userId;
+  final Game game;
+
+  GameDetailGuest({required this.signOut, required this.userId, required this.game});
+
+  @override
+  State<GameDetailGuest> createState() => _GameDetailGuestState();
+}
+
+class _GameDetailGuestState extends State<GameDetailGuest> {
+  final GameController gameController = GameController();
+  final _formKey = GlobalKey<FormState>();
+
+  void signOut() {
+    setState(() {
+      widget.signOut();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Game Detail',
+          style: TextStyle(
+            color: Color.fromARGB(255, 0, 0, 0),
+            fontFamily: 'Lexend',
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              signOut();
+            },
+            icon: Icon(Icons.lock_open),
+          )
+        ],
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Name: ${widget.game.name}',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Lexend',
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Description: ${widget.game.description}',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Lexend',
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Release Date: ${widget.game.releaseDate}',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Lexend',
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Id: ${widget.game.id}',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Lexend',
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _createReviewBtn() {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ReviewPage(signOut: signOut, game: widget.game, userId: widget.userId)),
+          );
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 109, 49, 237),
+      ),
+      child: const SizedBox(
+        width: double.infinity,
+        child: Text(
+          "Review",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, fontFamily: 'Lexend', color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _editBtn() {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => EditGamePage(signOut: signOut, game: widget.game, userId: widget.userId)),
+          );
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 109, 49, 237),
+      ),
+      child: const SizedBox(
+        width: double.infinity,
+        child: Text(
+          "Edit",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, fontFamily: 'Lexend', color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
--- game_tracker/lib/pages/guest_dashboard.dart
+++ game_tracker/lib/pages/guest_dashboard.dart
@@ -28,7 +28,6 @@ class _Dashboard extends State<Dashboard> {
   final GameGenreController gameGenreController = GameGenreController();
   final GenreController genreController = GenreController();
   List<Game> userGames = [];
-  List<Game> guestGames = [];
   List<Genre> genres = []; // Lista de gêneros
   Genre? selectedGenre; // Gênero selecionado
 
@@ -36,7 +35,6 @@ class _Dashboard extends State<Dashboard> {
   void initState() {
     super.initState();
     _loadUserGames();
-    _loadGuestGames();
     _loadGenres(); // Carregar gêneros
   }
 
@@ -46,22 +44,14 @@ class _Dashboard extends State<Dashboard> {
     });
   }
 
-  Future<void> _loadGuestGames() async {
-    var games = await gameController.getAllGames();
-    setState(() {
-      guestGames = games;
-    });
-  }
-
   Future<void> _loadUserGames() async {
-    var games = await gameController.getGamesByUserId(widget.userId);
+    var games = await gameController.getAllGames();
     setState(() {
       userGames = games;
     });
   }
 
   Future<void> _loadGenres() async {
-    // Supondo que você tenha um método para buscar os gêneros
     var genresList = await genreController.getAllGenres();
     setState(() {
       genres = genresList;
@@ -86,18 +76,10 @@ class _Dashboard extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
-    switch (widget.userId) {
-      case 0:
-        return Scaffold(
-          appBar: _buildAppBarGuest(),
-          body: _buildBodyGuest(),
-        );
-      default:
-        return Scaffold(
-          appBar: _buildAppBar(),
-          body: _buildBody(),
-        );
-    }
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
   }
 
   _buildAppBar() {
@@ -115,33 +97,6 @@ class _Dashboard extends State<Dashboard> {
         IconButton(
           onPressed: _refreshGames,
           icon: Icon(Icons.refresh),
-        ),
-        IconButton(
-          onPressed: () {
-            signOut();
-          },
-          icon: Icon(Icons.lock_open),
-        )
-      ],
-      centerTitle: true,
-    );
-  }
-
-  _buildAppBarGuest() {
-    return AppBar(
-      title: const Text(
-        'Dashboard',
-        style: TextStyle(
-          color: Color.fromARGB(255, 0, 0, 0),
-          fontFamily: 'Lexend',
-          fontSize: 22,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      actions: <Widget>[
-        IconButton(
-          onPressed: _refreshGamesGuest,
-          icon: Icon(Icons.refresh),
         )
       ],
       centerTitle: true,
@@ -150,10 +105,6 @@ class _Dashboard extends State<Dashboard> {
 
   void _refreshGames() {
     _loadUserGames();
-  }
-
-  void _refreshGamesGuest() {
-    _loadGuestGames();
   }
 
   _buildBody() {
@@ -167,7 +118,7 @@ class _Dashboard extends State<Dashboard> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 const Text(
-                  'Your Games',
+                  'Recent Games',
                   style: TextStyle(
                     color: Color.fromARGB(255, 0, 0, 0),
                     fontFamily: 'Lexend',
@@ -223,85 +174,6 @@ class _Dashboard extends State<Dashboard> {
                 },
               ),
             ),
-            const SizedBox(height: 20),
-            _createBtn(),
-            const SizedBox(height: 20),
-            _removeBtn(),
-            const SizedBox(height: 20),
-            _reviewsBtn(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _buildBodyGuest() {
-    return Padding(
-      padding: const EdgeInsets.all(60.0),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Recent Games',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    fontFamily: 'Lexend',
-                    fontSize: 27,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: _showSearchDialogGuest,
-                      color: Color.fromARGB(255, 109, 49, 237),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: guestGames.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      _navigateToGameDetailsGuest(guestGames[index]);
-                    },
-                    child: ListTile(
-                      title: Text(
-                        guestGames[index].name,
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontFamily: 'Lexend',
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: FutureBuilder<double>(
-                        future: reviewController.getGameScoreAvg(guestGames[index].id ?? 0),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState == ConnectionState.waiting) {
-                            return Text('Loading...');
-                          } else if (snapshot.hasError) {
-                            return Text('Error: ${snapshot.error}');
-                          } else {
-                            double averageScore = snapshot.data ?? 0.0;
-                            return Text('Score Average: ${averageScore.toStringAsFixed(2)}');
-                          }
-                        },
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
 
             const SizedBox(height: 20),
             _reviewsBtn(),
@@ -312,15 +184,6 @@ class _Dashboard extends State<Dashboard> {
   }
 
   void _navigateToGameDetails(Game game) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => GameDetail(signOut: signOut, game: game, userId: widget.userId),
-      ),
-    );
-  }
-
-  void _navigateToGameDetailsGuest(Game game) {
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -415,43 +278,6 @@ class _Dashboard extends State<Dashboard> {
                 if (game != null) {
                   Navigator.pop(context); // Close the dialog
                   _navigateToGameDetails(game);
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Game not found!')),
-                  );
-                }
-              },
-              child: Text('Search'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _showSearchDialogGuest() {
-    TextEditingController searchController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Search Game by name'),
-          content: TextField(
-            controller: searchController,
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              hintText: 'Enter game name',
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                String? gameName = searchController.text;
-                Game? game = await gameController.getGameByName(gameName);
-                if (game != null) {
-                  Navigator.pop(context); // Close the dialog
-                  _navigateToGameDetailsGuest(game);
                 } else {
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(content: Text('Game not found!')),
